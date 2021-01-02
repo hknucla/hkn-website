ac7639052d0e4e57b3068f2e541f614343c6176f
Kayla Song <kayla.song1@gmail.com>
2021-01-02 14:23:46 -0800

    strong basics for testbank

--- hkn-frontend/src/Components/TestBank/TestBank.js
+++ hkn-frontend/src/Components/TestBank/TestBank.js
@@ -1,10 +1,8 @@
 import React, {Component,} from "react";
-import TEST from "./hw1-student.pdf";
 import TESTBANKHEADERIMG from "./testbank.jpg";
 import "./TestBank.scss"
 import { Document, Page, pdfjs } from "react-pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import TestViewer from "./TestViewer";
 
 export default class TestBank extends Component{
 
@@ -23,20 +21,8 @@ export default class TestBank extends Component{
                     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
                     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
-                <div className="ViewTests">
-                    
-                </div>
+                <TestViewer/>
             </div>
-
-            // <div onContextMenu={(e) => e.preventDefault()} className="pdf-container">
-            //     <Document file={TEST} className="viewer">
-            //         <Page pageNumber={1} className="page" width="800"/>
-            //         <Page pageNumber={2} className="page" width="800" />
-            //         <Page pageNumber={3} className="page" width="800" />
-            //         <Page pageNumber={4} className="page" width="800" />
-            //         <Page pageNumber={5} className="page" width="800" />
-            //     </Document>
-            // </div>
         );
     }
 } 
